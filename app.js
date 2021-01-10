be7317036fbdc4d23830a5bd0b770e967ee0ee93
Adanna-bit <73751788+Adanna-bit@users.noreply.github.com>
2021-01-10 13:30:09 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+document.querySelector('#red').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'red'
+}
+document.querySelector('#orange').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'orange'
+}
+document.querySelector('#yellow').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'yellow'
+}
+document.querySelector('#green').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'green'
+}
+document.querySelector('#blue').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'blue'
+}
+document.querySelector('#purple').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'purple'
+}
+document.querySelector('#brown').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'brown'
+}
+document.querySelector('#grey').onclick = function () {
+	document.querySelector('body').style.backgroundColor= 'grey'
+}
